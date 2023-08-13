4dc003b7caa03ab9a3ac480fcc5277defd7d6b8c
Borja Lorenzo <plushyzeus35@gmail.com>
2023-08-13 15:31:26 +0200

    config environment variables

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
     database: {
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_DATABASE,
+        password: process.env.PERSONALAPP_PASSWORD,
+        database: process.env.PERSONALAPP_DATABASE,
         host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USER,
+        user: process.env.PERSONALAPP_USER,
         port: 3306
     },
     email: {
         password: process.env.EMAIL_PASSWORD,
-        receiver: process.env.EMAIL_RECEIVER
+        receiver: 'plushyzeus35@gmail.com'
     }
 }
