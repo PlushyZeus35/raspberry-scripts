0d8b9303d99ddeb7546558478ef2aa01be04d3ba
Borja Lorenzo <plushyzeus35@gmail.com>
2023-12-05 20:33:48 +0100

    fix date format in telegram message

--- notifyBirthday.py
+++ notifyBirthday.py
@@ -9,7 +9,7 @@ for birth in birthdays:
         todaysBirthdays.append(birth)
 
 if len(todaysBirthdays) > 0:
-    telegramMessage = f'<strong>Cumpleaños de hoy {date.today().strftime('%d/%m')}</strong>\n'
+    telegramMessage = f'<strong>Cumpleaños de hoy {date.today().strftime('%d %m')}</strong>\n'
     for birth in todaysBirthdays:
         telegramMessage += f'{birth.name} cumple {birth.age} años.\n'
     TelegramUtils.sendMessage(telegramMessage)
